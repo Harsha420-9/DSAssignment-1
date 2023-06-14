a3105eb7f0aa6e731cf415d7b9bccd3498e3e3dc
Harsha420-9 <68031165+Harsha420-9@users.noreply.github.com>
2023-06-14 18:01:12 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+# The Fibonacci Sequence is the series of numbers :
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+
+# Every next number is found by adding up the two numbers before it.
+
+# Expected Output : 1 1 2 3 5 8 13 21 34
+
+sum = 0
+n1,n2 = 0,1
+num = 9
+for i in range(0,num):
+    n1=n2
+    n2=sum
+    sum=n1+n2
+    print(sum)
+if num<=0:
+    print('enter number greater than zero ')
