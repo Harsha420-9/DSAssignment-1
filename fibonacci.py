f31e026c959112d31218c662f8e9bcf5df0cc70f
Harsha420-9 <68031165+Harsha420-9@users.noreply.github.com>
2023-06-16 21:28:22 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -6,13 +6,8 @@
 
 # Expected Output : 1 1 2 3 5 8 13 21 34
 
-sum = 0
-n1,n2 = 0,1
-num = 50
-for i in range(0,num):
-    n1=n2
-    n2=sum
-    sum=n1+n2
-    print(sum)
-if num<=0:
-    print('enter number greater than zero ')
+num1 = 0
+num2 = 1
+while num2<= 50:
+    print(num2)
+    num1,num2 = num2,num1+num2
